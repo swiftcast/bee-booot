9ecba5526747ba66c0febd69a3f9b834c8e0fc24
swiftcast <4298986+swiftcast@users.noreply.github.com>
2024-04-20 01:16:57 -0400

    Add warning message to image listener

--- src/listeners/ImageMessageOnlyListener.ts
+++ src/listeners/ImageMessageOnlyListener.ts
@@ -18,6 +18,8 @@ export class UserListener extends Listener {
         if (!hasImage) {
             try {
                 await message.delete();
+                const warningMessage = await message.channel.send("Only images are allowed in this channel. Yapping must be conducted elsewhere. Thank you for your understanding. 🐝");
+                setTimeout(() => warningMessage.delete().catch(err => this.container.logger.error(`Failed to delete the warning message: ${err}`)), 30000);
                 this.container.logger.info(`Deleted a text-only message from ${message.author.tag}`);
             } catch (error) {
                 this.container.logger.error(`Failed to delete message: ${error}`);
