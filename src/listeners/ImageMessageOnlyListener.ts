d515a63f73044204e5b9ef03b6488323d1be1a27
swiftcast <4298986+swiftcast@users.noreply.github.com>
2024-04-22 02:08:12 -0400

    Update listener to acccess image ids array

--- src/listeners/ImageMessageOnlyListener.ts
+++ src/listeners/ImageMessageOnlyListener.ts
@@ -1,24 +1,34 @@
 import { Events, Listener, ListenerOptions } from '@sapphire/framework';
 import { Message } from 'discord.js';
 import { ApplyOptions } from '@sapphire/decorators';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @ApplyOptions<ListenerOptions>({
-	event: Events.MessageCreate,
-	once: false
+    event: Events.MessageCreate,
+    once: false
 })
 export class UserListener extends Listener {
-	// Specify the channel ID to listen to; configurable as needed
-	private readonly TARGET_CHANNEL_ID = "1231015705344868462";
+    private configPath = path.join(process.cwd(), 'config.json');
+    private targetChannelIds: string[] = this.loadConfig();
 
-	public async run(message: Message) {
-        if (message.channel.id !== this.TARGET_CHANNEL_ID || message.author.bot) return;
+    private loadConfig(): string[] {
+        if (fs.existsSync(this.configPath)) {
+            const config = JSON.parse(fs.readFileSync(this.configPath, 'utf-8'));
+            return config.imageOnlyChannelIds || [];
+        }
+        return [];
+    }
+
+    public async run(message: Message) {
+        if (!this.targetChannelIds.includes(message.channel.id) || message.author.bot) return;
 
         const hasImage = message.attachments.some(attachment => this.isImageAttachment(attachment.contentType));
 
         if (!hasImage) {
             try {
                 await message.delete();
-                const warningMessage = await message.channel.send("Only images are allowed in this channel. Yapping must be conducted elsewhere. Thank you for your understanding. 🐝");
+                const warningMessage = await message.channel.send("Only images are allowed in this channel. Please adhere to the channel rules. Thank you for your understanding. 🐝");
                 setTimeout(() => warningMessage.delete().catch(err => this.container.logger.error(`Failed to delete the warning message: ${err}`)), 30000);
                 this.container.logger.info(`Deleted a text-only message from ${message.author.tag}`);
             } catch (error) {
